77617c781ef94bdbcf498a9f559d0811288fe1a3
raisassc <sakila.raisa@gmail.com>
2024-09-04 21:29:15 +0700

    membuat proyek django raisa petshop

---
+++ raisa_petshop/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for raisa_petshop project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'raisa_petshop.settings')
+
+application = get_asgi_application()
